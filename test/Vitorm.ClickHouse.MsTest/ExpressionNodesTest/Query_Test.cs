addb6148ac03324a2b7b2599c88b1873c29a597e
Lith <LithWang@outlook.com>
2024-08-22 22:48:47 +0800

    rename

--- test/Vitorm.ClickHouse.MsTest/ExpressionNodesTest/Query_Test.cs
+++ test/Vitorm.ClickHouse.MsTest/ExpressionNodesTest/Query_Test.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using Vit.Linq.ExpressionNodes.ExpressionTreeTest;
+using Vit.Linq.ExpressionNodes.ExpressionNodesTest;
 
 namespace Vitorm.MsTest.ExpressionTreeTest
 {
